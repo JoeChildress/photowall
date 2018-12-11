bb5e5b40a95d82931d4a719a8276b8d45a47c766
Childress <childjo@ds.ad.adp.com>
2018-12-10 17:03:19 -0800

    props overview

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ class List extends Component {
     render() {
         return (
             <ul>
-                {tasks.map((task) => <li key={task}>{task}</li>)}
+                {this.props.tasks.map((task) => <li key={task}>{task}</li>)}
             </ul>
         );
     }
@@ -15,15 +15,16 @@ class List extends Component {
 
 class Header extends Component {
     render() {
-        return <h1>My List</h1>
+        return <h1>{this.props.title}</h1>
     }
 }
 
 class Main extends Component {
     render() {
         return  <div>
-                    <Header />
-                    <List />
+                    <Header title={'My Props Title'} />
+                    <List tasks={['thing one','thing two']}/>
+										<List tasks={['cat one','cat two']}/>
                 </div>
     }
 }
