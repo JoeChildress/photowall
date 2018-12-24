dfcccb297301a9903da269a5a69389d93535ce72
Joseph Childress <josephchildress@Josephs-MacBook-Pro.local>
2018-12-23 18:38:57 -0800

    added onClick to remove photos

--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 import Photo from './Photo'
 
-class PhotoWall extends Component {
+class PhotoWall extends Component { 
 	render() {
 		return <div className="photoGrid">
-							{this.props.posts.map((post, index) => <Photo key={index} post={post}/>)}
+							{this.props.posts.map((post, index) => <Photo key={index} post={post} onRemovePhoto={this.props.onRemovePhoto} />)}
 						</div>
 	}
 }
